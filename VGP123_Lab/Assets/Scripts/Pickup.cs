2866f5678e2bc8f09fae6d4ab5e97a464e3a33de
terishrimp <72040129+terishrimp@users.noreply.github.com>
2021-02-20 15:17:26 -0500

    Adding Charge Animation

--- VGP123_Lab/Assets/Scripts/Pickup.cs
+++ VGP123_Lab/Assets/Scripts/Pickup.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     [SerializeField] AudioClip pickupNoise = null;
+
+    [SerializeField] PlayerMovement player = null;
+
+    GameObject collectableList;
+    private void Start()
+    { 
+        if(GameObject.FindGameObjectWithTag("collectableList") != null) { 
+        collectableList = GameObject.FindGameObjectWithTag("collectableList");
+        }
+        else
+        {
+            var newList = Instantiate(new GameObject("Collectables"), new Vector3(0, 0, 0), Quaternion.identity);
+            newList.tag = "collectableList";
+            collectableList = newList;
+        }
+
+        transform.SetParent(collectableList.transform);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerMovement>() != null) {
